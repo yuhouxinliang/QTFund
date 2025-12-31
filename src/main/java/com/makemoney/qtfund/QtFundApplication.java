b6566365ce640dfa38ccb10bfdc939038b1f5ff7
11404 <zhuyixin0216@gmail.com>
2025-12-31 21:34:39 +0800

    项目初始化

---
+++ src/main/java/com/makemoney/qtfund/QtFundApplication.java
@@ -0,0 +1,13 @@
+package com.makemoney.qtfund;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QtFundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QtFundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/makemoney/qtfund/QtFundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makemoney.qtfund;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QtFundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
